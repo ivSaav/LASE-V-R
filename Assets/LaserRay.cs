cda162e1b4b5f390da425da1c5500c1dfa346718
Ivo Saavedra <ivo_saavedra@outlook.com>
2022-11-20 23:13:58 +0000

    Add: different laser logic

--- Assets/LaserRay.cs
+++ Assets/LaserRay.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,22 +10,71 @@ public class LaserRay : MonoBehaviour {
     [SerializeField] private Transform laserStartPoint;
 
     [SerializeField] private int maxBounces = 10;
-    // Start is called before the first frame update
-    void Start() {
-        lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.SetPosition(0, laserStartPoint.position);
+
+    private RaycastHit hit;
+
+    private void Awake()
+    {
+        lineRenderer = this.GetComponent<LineRenderer>();
     }
 
+    // // Start is called before the first frame update
+    // void Start() {
+    //     lineRenderer = GetComponent<LineRenderer>();
+    //     lineRenderer.SetPosition(0, laserStartPoint.position);
+    // }
+
     // Update is called once per frame
-    void Update() {
-        RaycastLaser(transform.position, -transform.right);
+    void Update()
+    {
+
+        var ray = new Ray(laserStartPoint.position, -laserStartPoint.right);
+
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, laserStartPoint.position);
+
+        for (var i = 0; i < maxBounces; i++)
+        {
+            // check collision
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, 100f))
+            {
+                if (hit.collider.CompareTag("Reflectable"))
+                {
+                    // add new position to line renderer
+                    lineRenderer.positionCount += 1;
+                    
+                    // render position at line hit point
+                    lineRenderer.SetPosition(lineRenderer.positionCount-1, hit.point);
+
+                    // direction of ray is the angle between the vector and the hit object
+                    ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+                }
+                else
+                {
+                    // TODO: handle non reflection hits
+                    break;
+                }
+            }
+            else
+            {              
+                // add new position to line renderer
+                lineRenderer.positionCount += 1;
+
+                // line renderer position has the same origin and direction as the ray
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, ray.origin + ray.direction * 100f);
+                break;
+            }
+        }
+        // RaycastLaser(transform.position, -transform.right);
     }
 
     void RaycastLaser(Vector3 position, Vector3 direction) {
         lineRenderer.SetPosition(0, laserStartPoint.position);
 
+        var ray = new Ray(position, direction);
         for (var bounceIndex = 1; bounceIndex <= maxBounces; bounceIndex++) {
-            if (Physics.Raycast(position, direction, out var laserHit)) {
+            if (Physics.Raycast(position, ray.direction, out var laserHit)) {
+                
                 position = laserHit.point;
                 direction = Vector3.Reflect(direction, laserHit.normal);
                 
